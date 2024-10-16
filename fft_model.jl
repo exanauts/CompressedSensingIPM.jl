3d5cfad700f3d682c9f5ffb074155a1eca4e9d5c
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-10-16 11:25:59 -0500

    Update fft_model.jl

--- fft_model.jl
+++ fft_model.jl
@@ -1,10 +1,5 @@
 using NLPModels
 
-mutable struct FFTNLPModel <: AbstractNLPModel{Float64, Vector{Float64}}
-	meta::NLPModelMeta{Float64, Vector{Float64}}
-	counters::Counters
-end
-
 mutable struct FFTParameters
 	paramB
 	eps_NT
@@ -17,6 +12,12 @@ function FFTParameters(DFTdim, DFTsize, M_perptz, lambda, index_missing, alpha_L
     paramLS = (alpha_LS, gamma_LS)
     paramB = (eps_barrier, mu_barrier)
     FFTParameters(paramB, eps_NT, paramLS, paramf)
+end
+
+mutable struct FFTNLPModel <: AbstractNLPModel{Float64, Vector{Float64}}
+	meta::NLPModelMeta{Float64, Vector{Float64}}
+	parameters::FFTParameters
+	counters::Counters
 end
 
 meta = NLPModelMeta(
@@ -68,16 +69,35 @@ end
 
 function NLPModels.obj(nlp::FFTNLPModel, x::AbstractVector)
   increment!(nlp, :neval_obj)
-  ...
+    DFTdim = nlp.parameters.paramf[1]
+    DFTsize = nlp.parameters.paramf[2]
+    M_perptz = nlp.parameters.paramf[3]
+    lambda = nlp.parameters.paramf[4]
+    index_missing = nlp.parameters.paramf[5]
+	# Mt = nlp.parameters.paramf[6]
+
+    # c should be in the model?
+    fval = 0.5 * sum((M_perp_beta_old(DFTdim, DFTsize, beta, index_missing)).^2) - beta' * M_perptz + lambda * sum(c)
+    return fval
   return res
 end
 
 function NLPModels.grad!(nlp::FFTNLPModel, x::AbstractVector, g::AbstractVector)
   increment!(nlp, :neval_grad)
-  ...
+  DFTdim = nlp.parameters.paramf[1]
+  DFTsize = nlp.parameters.paramf[2]
+  M_perptz = nlp.parameters.paramf[3]
+  lambda = nlp.parameters.paramf[4]
+  index_missing = nlp.parameters.paramf[5]
+  # Mt = nlp.parameters.paramf[6]
+
+  n = prod(DFTdim)
+  gradb = view(g, 1:n)
+  gradc = view(g, n+1:2*n)
+  gradb .= M_perpt_M_perp_vec_old(DFTdim, DFTsize, beta, index_missing).-Mperptz)
+  gradc .= lambda .* ones(n)
   return g
 end
-
 
 function NLPModels.hprod!(
   nlp::FFTNLPModel,
