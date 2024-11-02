92983012f51dd609a1441e26b3e125c582a956a8
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-11-02 14:10:12 -0500

    Fix fft_model.jl

--- fft_model.jl
+++ fft_model.jl
@@ -20,7 +20,6 @@ end
 mutable struct FFTNLPModel <: AbstractNLPModel{Float64, Vector{Float64}}
     meta::NLPModelMeta{Float64, Vector{Float64}}
     parameters::FFTParameters
-    c::Vector{Float64}
     N::Int
     counters::Counters
 end
@@ -33,7 +32,6 @@ function FFTNLPModel(parameters::FFTParameters)
     ncon = 2 * N
     x0 = zeros(Float64, nvar)
     y0 = zeros(Float64, ncon)
-    c = zeros(Float64, ncon)
     lvar = -Inf * ones(Float64, nvar)
     for i = N+1:2N
       lvar[i] = zero(Float64)  # cᵢ ≥ 0
@@ -56,7 +54,7 @@ function FFTNLPModel(parameters::FFTParameters)
         islp = false,
         name = "CompressedSensing-$(DFTdim)D",
     )
-    return FFTNLPModel(meta, parameters, c, N, Counters())
+    return FFTNLPModel(meta, parameters, N, Counters())
 end
 
 function NLPModels.cons!(nlp::FFTNLPModel, x::AbstractVector, c::AbstractVector)
@@ -78,8 +76,8 @@ function NLPModels.jprod!(
   increment!(nlp, :neval_jprod)
   N = nlp.N
   for i = 1:N
-    Jv[i]     = -v[i] - v[i+N]
-    Jv[N+i]   =  v[i] - v[i+N]
+    Jv[i]   = -v[i] - v[i+N]
+    Jv[N+i] =  v[i] - v[i+N]
   end
   return Jv
 end
@@ -109,9 +107,10 @@ function NLPModels.obj(nlp::FFTNLPModel, x::AbstractVector)
     # Mt = nlp.parameters.paramf[6]
 
     fft_val = M_perp_beta_wei(DFTdim, DFTsize, x, index_missing)
-    n = prod(DFTsize)
-    beta = view(x, 1:n)
-    fval = 0.5 * dot(fft_val, fft_val) - dot(beta, M_perptz) + lambda * sum(nlp.c)
+    N = nlp.N
+    beta = view(x, 1:N)
+    c = view(x, N+1:2*N)
+    fval = 0.5 * dot(fft_val, fft_val) - dot(beta, M_perptz) + lambda * sum(c)
     return fval
 end
 
