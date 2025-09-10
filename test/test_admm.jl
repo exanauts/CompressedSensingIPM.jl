c6d74f7ed744f74c9d4de2f8c67f68e5bfe4dca6
Vishwas Rao <vhebbur@moonshot.cels.anl.gov>
2025-09-10 13:50:31 -0500

    test cases for ADMM

--- test/test_admm.jl
+++ test/test_admm.jl
@@ -68,7 +68,7 @@ if dim1
   for N in (100, 200, 500)
     for rdft in (false, true)
       @testset "1D -- ADMM -- CPU -- rdft=$rdft -- $N" begin
-        nlp, solver, results = admm_example_1D(N; gpu=false, rdft, check=true)
+        solution, timer = admm_example_1D(N; gpu=false, rdft, check=true)
       end
     end
   end
